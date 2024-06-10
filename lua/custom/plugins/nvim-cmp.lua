447d138e5c6fda52c079a1375b0e0a13a7d19361
MarcHamamji <hamamjimarc@gmail.com>
2024-06-10 15:53:25 +0300

    Add borders in cmp windows

--- lua/custom/plugins/nvim-cmp.lua
+++ lua/custom/plugins/nvim-cmp.lua
@@ -36,6 +36,10 @@ return {
                     luasnip.lsp_expand(args.body)
                 end,
             },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
             mapping = cmp.mapping.preset.insert {
                 ['<C-Space>'] = cmp.mapping.complete {},
                 ['<CR>'] = cmp.mapping.confirm { select = false },
