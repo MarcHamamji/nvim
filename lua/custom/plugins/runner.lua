f29240b469864a890a945d23685d6b1442b59288
Marc Hamamji <hamamjimarc@gmail.com>
2024-12-17 14:30:25 +0200

    Add java support for runner

--- lua/custom/plugins/runner.lua
+++ lua/custom/plugins/runner.lua
@@ -4,12 +4,24 @@ return {
     'MarcHamamji/runner.nvim',
     config = function()
         local runner = require('runner')
+        local helpers = require('runner.handlers.helpers')
 
         runner.setup {
             position = 'right',
             width = 60,
             height = 10,
         }
+
+        runner.set_handler('java', function(buffer)
+            local current_file = vim.fn.expand('%')
+
+            local handlers = {
+                ['Run current file'] = helpers.shell_handler('java ' .. current_file),
+                ['Custom'] = helpers.shell_handler('java ' .. current_file, true),
+            }
+
+            helpers.choice(handlers)(buffer)
+        end)
 
         utils.map('n', '<leader><space>', runner.run, { desc = 'Runner' })
     end,
