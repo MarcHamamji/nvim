c6c4d46c85753942d00256d41a7bbecd7664b4a5
Marc Hamamji <hamamjimarc@gmail.com>
2024-12-17 14:43:33 +0200

    Use tokyonight

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,8 +1,8 @@
 return {
     {
-        'catppuccin/nvim',
+        'folke/tokyonight.nvim',
         config = function()
-            vim.cmd.colorscheme('catppuccin')
+            vim.cmd.colorscheme('tokyonight')
         end,
     },
     {
