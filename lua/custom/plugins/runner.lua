ced0b800b50ac9b03c9f51158b31ff25c60643a5
Marc Hamamji <hamamjimarc@gmail.com>
2024-12-18 18:54:59 +0200

    Remove java handler for runner

--- lua/custom/plugins/runner.lua
+++ lua/custom/plugins/runner.lua
@@ -5,24 +5,12 @@ return {
     keys = { { '<leader><Space>' } },
     config = function()
         local runner = require('runner')
-        local helpers = require('runner.handlers.helpers')
 
         runner.setup {
             position = 'right',
             width = 60,
             height = 10,
         }
-
-        runner.set_handler('java', function(buffer)
-            local current_file = vim.fn.expand('%')
-
-            local handlers = {
-                ['Run current file'] = helpers.shell_handler('java ' .. current_file),
-                ['Custom'] = helpers.shell_handler('java ' .. current_file, true),
-            }
-
-            helpers.choice(handlers)(buffer)
-        end)
 
         utils.map('n', '<leader><Space>', runner.run, { desc = 'Runner' })
     end,
