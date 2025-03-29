94b26e5771d69ddd73429320f735811f53846f15
Marc Hamamji <hamamjimarc@gmail.com>
2025-03-29 15:47:41 +0200

    Add Tex handler

--- lua/custom/plugins/runner.lua
+++ lua/custom/plugins/runner.lua
@@ -5,12 +5,17 @@ return {
     keys = { { '<leader><Space>' } },
     config = function()
         local runner = require('runner')
+        local helpers = require('runner.handlers.helpers')
 
         runner.setup {
             position = 'right',
             width = 60,
             height = 10,
         }
+
+        runner.set_handler('tex', function (bufnr)
+            helpers.shell_handler('pdflatex --output-directory=out ' .. vim.fn.expand('%'))(bufnr)
+        end)
 
         utils.map('n', '<leader><Space>', runner.run, { desc = 'Runner' })
     end,
