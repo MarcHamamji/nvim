bde57854fd71c8b5dfdcc2bf845e697154a13e9b
Marc Hamamji <hamamjimarc@gmail.com>
2025-07-29 15:18:31 +0300

    Update telescope config

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,27 +1,19 @@
 local utils = require('custom.utils')
 
--- wait for this issue to be solved:
--- https://github.com/nvim-telescope/telescope.nvim/issues/3436
-vim.api.nvim_create_autocmd("User", {
-  pattern = "TelescopeFindPre",
-  callback = function()
-    vim.opt_local.winborder = "none"
-    vim.api.nvim_create_autocmd("WinLeave", {
-      once = true,
-      callback = function()
-        vim.opt_local.winborder = "rounded"
-      end,
-    })
-  end,
-})
 
 return {
     'nvim-telescope/telescope.nvim',
     dependencies = {
         'nvim-lua/plenary.nvim',
         'kyazdani42/nvim-web-devicons',
+        {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'make'
+        }
     },
     config = function()
+        require('telescope').load_extension('fzf')
+
         local builtins = require('telescope.builtin')
         local themes = require('telescope.themes')
 
@@ -40,6 +32,8 @@ return {
         end, { desc = 'Current buffer fuzzy find' })
 
         utils.map('n', '<leader>sg', builtins.live_grep, { desc = '[S]earch [g]rep' })
+        utils.map('v', '<leader>sg', builtins.grep_string, { desc = '[S]earch [g]rep' })
+        utils.map('n', '<leader>sb', builtins.current_buffer_fuzzy_find, { desc = '[S]earch [b]uffer' })
         utils.map('n', '<leader>sk', builtins.keymaps, { desc = '[S]earch [k]eymaps' })
         utils.map('n', '<leader>sh', builtins.help_tags, { desc = '[S]earch [h]elp tags' })
         utils.map('n', '<leader>sm', builtins.man_pages, { desc = '[S]earch [m]an pages' })
@@ -47,6 +41,6 @@ return {
         utils.map('n', '<leader>so', builtins.oldfiles, { desc = '[S]earch [o]ld files' })
         utils.map('n', '<leader>ss', builtins.lsp_document_symbols, { desc = '[S]earch document [s]ymbols' })
         utils.map('n', '<leader>sw', builtins.lsp_dynamic_workspace_symbols, { desc = '[S]earch [w]orkspace symbols' })
-        utils.map('n', '<leader>sr', builtins.resume, { desc = '[S]earch [r]resume' })
+        utils.map('n', '<leader>sr', builtins.resume, { desc = '[S]earch [r]esume' })
     end,
 }
