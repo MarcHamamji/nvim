8f9aa2a3b3bd279d84d70a37dc23f2f8f58bbde3
MarcHamamji <hamamjimarc@gmail.com>
2026-06-28 16:26:50 +0300

    Add blink

--- lua/custom/ui.lua
+++ lua/custom/ui.lua
@@ -1,27 +1,60 @@
 vim.cmd [[ colorscheme retrobox ]]
+
 vim.cmd [[ hi! link Pmenu Normal ]]
 vim.cmd [[ hi! link PmenuKind Pmenu ]]
 vim.cmd [[ hi! link PmenuExtra Pmenu ]]
+
 vim.cmd [[ hi! link TreesitterContext CursorLine ]]
+
 vim.cmd [[ hi! link NeogitDiffAdd DiffAdd ]]
 vim.cmd [[ hi! link NeogitDiffAddHighlight DiffAdd ]]
 vim.cmd [[ hi! link NeogitDiffDelete DiffDelete ]]
 vim.cmd [[ hi! link NeogitDiffDeleteHighlight DiffDelete ]]
+
 vim.cmd [[ hi! Title gui=bold guifg=Orange ]]
 
+vim.cmd [[ hi Search    guibg=#5a7a5a ctermbg=65 ]]
+vim.cmd [[ hi IncSearch guibg=#8f6330 ctermbg=130 ]]
+
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-      vim.highlight.on_yank({timeout = 50})
-  end,
+    callback = function()
+        vim.highlight.on_yank({ timeout = 50 })
+    end,
 })
-
-
-local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl })
-end
 
 vim.diagnostic.config {
     virtual_text = true
 }
+
+vim.pack.add { {
+    src = "https://github.com/saghen/blink.cmp",
+} }
+
+require("blink.cmp").setup {
+    signature = {
+        enabled = true
+    },
+    fuzzy = {
+        prebuilt_binaries = {
+            force_version = 'v1.9.1'
+        }
+    },
+    completion = {
+        list = {
+            selection = {
+                preselect = false,
+                auto_insert = true
+            }
+        },
+        menu = {
+            draw = {
+                treesitter = { 'lsp' }
+            }
+        },
+        documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 0,
+        }
+    }
+}
+
