499a576ea1ddd64763052fdbdcc4efe8d19d552a
MarcHamamji <hamamjimarc@gmail.com>
2026-06-28 16:27:34 +0300

    Add treesitter auto install

--- lua/custom/treesitter.lua
+++ lua/custom/treesitter.lua
@@ -1,19 +1,38 @@
 vim.pack.add { {
     src = "https://github.com/nvim-treesitter/nvim-treesitter",
-    version = "master"
+    version = "main"
 } }
 
-require('nvim-treesitter.configs').setup {
-    ensure_installed = {},
-    sync_install = false,
-
+require('nvim-treesitter').setup {
+    ensure_installed = { "comment" },
     auto_install = true,
-    ignore_install = {},
-
     highlight = {
         enable = true,
     },
 }
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "*" },
+  callback = function(args)
+    local ft = vim.bo[args.buf].filetype
+    local lang = vim.treesitter.language.get_lang(ft)
+
+    if not vim.treesitter.language.add(lang) then
+      local available = vim.g.ts_available
+        or require("nvim-treesitter").get_available()
+      if not vim.g.ts_available then
+        vim.g.ts_available = available
+      end
+      if vim.tbl_contains(available, lang) then
+        require("nvim-treesitter").install(lang)
+      end
+    end
+
+    if vim.treesitter.language.add(lang) then
+      vim.treesitter.start(args.buf, lang)
+    end
+  end,
+})
 
 vim.pack.add {
     'https://github.com/nvim-treesitter/nvim-treesitter-context'
@@ -22,11 +41,11 @@ vim.pack.add {
 require('treesitter-context').setup{
   enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
   multiwindow = false, -- Enable multiwindow support.
-  max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+  max_lines = 8, -- How many lines the window should span. Values <= 0 mean no limit.
   min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
   line_numbers = true,
   multiline_threshold = 20, -- Maximum number of lines to show for a single context
-  trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  trim_scope = 'inner', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
   mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
   -- Separator between context and content. Should be a single character string, like '-'.
   -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
