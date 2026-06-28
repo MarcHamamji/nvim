ce2c665d96787c23a95ee34778524e1875509275
MarcHamamji <hamamjimarc@gmail.com>
2026-06-28 16:28:02 +0300

    Tweak some mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -6,17 +6,32 @@ vim.keymap.set("n", "<C-k>", ":cprev<CR>", { desc = "Go to previous quickfix ite
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 
-vim.keymap.set("n", "<C-j>", ":cnext<CR>", { desc = "Next quickfix item" })
-vim.keymap.set("n", "<C-k>", ":cprevious<CR>", { desc = "Previous quickfix item" })
+vim.keymap.set("n", "<C-j>", ":cnext<CR>zz", { desc = "Next quickfix item" })
+vim.keymap.set("n", "<C-k>", ":cprevious<CR>zz", { desc = "Previous quickfix item" })
 
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set("n", "H", function()
+    vim.cmd("normal! J")
+    local char = vim.fn.getline('.'):sub(vim.fn.col('.'), vim.fn.col('.'))
+    if char == " " then
+        vim.cmd("normal! x")
+    end
+end, { desc = "Join lines without adding a space" })
+
 vim.keymap.set("i", "<C-Space>", "<C-x><C-o>")
 
 vim.keymap.set("n", "<leader>t", function()
     vim.cmd[[vsplit]]
     vim.cmd[[term]]
-end)
+end, { desc = "Open [t]erminal split" })
+
+vim.keymap.set(
+    "n",
+    "<Leader>l",
+    ":let @+ = expand('%') . ':' . line('.')<cr>",
+    { desc = "Copy file path and [l]ine number", silent = true }
+)
